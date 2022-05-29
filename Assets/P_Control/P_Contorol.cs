f8a490517477e80fa4c41b634a6289618d8a58ae
Aiba_0325 <bantan.ab325@gmail.com>
2022-05-30 02:42:13 +0900

    エフェクト追加、

--- Assets/P_Control/P_Contorol.cs
+++ Assets/P_Control/P_Contorol.cs
@@ -5,38 +5,55 @@ using UnityEngine;
 public class P_Contorol : MonoBehaviour
 {
     //ˆÚ“®
-    public float p_h ;
-    [SerializeField] float p_movePower = 5f;
-    
-    [SerializeField] float moveSpeed;
+    public float _h ;   
+    [SerializeField] float _speed;
     [SerializeField] float StartSpeed;
     [SerializeField] float MinasStartSpeed;
-
+   
     
 
     //ƒWƒƒƒ“ƒv
-    //private bool OnGround;
+    private bool OnGround;
     private bool jump;
-  [SerializeField]   float jumpPower= 10f;
+    [SerializeField]   float jumpPower= 10f;
     [SerializeField] int jumpLimit=2;
     public int jumpCount=0;
 
     float p_ScaleX;
 
+
+    [SerializeField] float _attackmove1 = 5;
+    [SerializeField] Transform _crosshair;
+    
+
+
+
+
     Rigidbody2D rb;
     private Animator anim = null;
+    [SerializeField] Animator runEfect = null;
+    [SerializeField] Animator attackEffect = null;
+
+   
+
+
 
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+       
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        p_h = Input.GetAxisRaw("Horizontal");
-        FlipX(p_h);
+     {
+        float _h = Input.GetAxisRaw("Horizontal");
+        Move();
+        FlipX(_h);
+        Attack();
+        JudgeBool();
 
 
         if (Input.GetButtonDown("Jump"))
@@ -47,45 +64,109 @@ public class P_Contorol : MonoBehaviour
                 jump = true;
                
             }
-            
-
         }
-
 
     } 
 
 
      void FixedUpdate()
     {
-
-
-        if (rb.velocity.magnitude < 10)
-        {
-            rb.AddForce(Vector2.right * p_h * p_movePower, ForceMode2D.Force);
-        }
-
-        
-        if(p_h < 0 || p_h > 0)
-        {
-            anim.SetBool("run", true);
-        }
-        else {anim.SetBool("run", false);
- }
-
-        
-
         if (jump)
         {
             Debug.Log("BBB");
             rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("OnGround",false);
             jump = false;
+            OnGround = false;
+
             jumpCount++;
+           
+        }
+     
+         Vector2 dir = _crosshair.position - transform.position;
+        
+    }
+
+
+
+    void Attack()
+    {
+        //Vector3 tmp = GameObject.Find("CrossHair").transform.position;
+
+
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    rb.AddForce(aaa* _attackmove1, ForceMode2D.Impulse);
+        //}
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            attackEffect.SetTrigger("Attack");
+        }
+
+    }
+
+
+    //ˆÚ“®
+     void Move()
+    {
+        float _h = Input.GetAxisRaw("Horizontal");
+
+        Vector2 velo = new Vector2(_h * _speed, rb.velocity.y);
+        rb.velocity = velo;
+
+          
+        if (_h == 0)
+        {
+            anim.SetBool("run", false);
+            runEfect.SetBool("Run", false);
+        }
+        else
+        {
+         anim.SetBool("run", true);
+         runEfect.SetBool("Run", true);
+        }
+    }
+
+  
+
+    void JudgeBool()
+    {
+        if (rb.velocity.y == 0)
+        {
+            runEfect.SetBool("jump", false);
+        }
+        else
+        {
+            runEfect.SetBool("jump", true);
+        }
+
+        if (OnGround == true)
+        {
+            runEfect.SetBool("Ground", true);
+        }
+        else
+        {
+            runEfect.SetBool("Ground", false);
 
         }
 
-        
+        if (rb.velocity.y > 0)
+        {
+            anim.SetBool("jump", true);
+
+
+        }
+
+        if (rb.velocity.y <= 0)
+        {
+            anim.SetBool("jump", false);
+
+        }
 
     }
+
 
     // ŠG‚ÌU‚èŒü‚«
     void FlipX(float horizontal)
@@ -110,7 +191,9 @@ public class P_Contorol : MonoBehaviour
         
         if(collision.gameObject.tag =="Ground")
         {
+            anim.SetBool("OnGround", true);
             jumpCount = 0;
+            OnGround = true;
         }
 
     }
---
+++ Assets/RunEfect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunEfect : MonoBehaviour
+{
+    public Animation runEfect;
+
+    void Start() 
+    { 
+       runEfect = GetComponent<Animation>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+
+
+}
